79611c380be0b7807342dd5534b7546c12e84272
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-05 20:41:25 +0000

    Update message-mixer.js

--- Node.js/messageMixer/message-mixer.js
+++ Node.js/messageMixer/message-mixer.js
@@ -1,14 +1,11 @@
 // Import the functions from encryptors.js here.
-const encryptors = require('./encryptors.js');
-// Encryption Functions
-/////////////////////////////////////////////
-const { caesarCipher,symbolCipher,reverseCipher } = encryptors;
+const { caesarCipher, symbolCipher, reverseCipher } = require('./encryptors.js');
 
 // User Input / Output Logic
 /////////////////////////////////////////////
 
 const encryptionMethod = getEncryptionMethod();
-process.stdin.on('data', (userInput) => {
+process.stdin.on("data", (userInput) => {
   displayEncryptedMessage(encryptionMethod, userInput);
 });
 
@@ -16,33 +13,32 @@ process.stdin.on('data', (userInput) => {
 the user chose when they ran the program. */
 function getEncryptionMethod() {
   let encryptionMethod;
-  
-  const encryptionType = process.argv[2];  
-  if (encryptionType === 'symbol') {
+
+  const encryptionType = process.argv[2];
+  if (encryptionType === "symbol") {
     encryptionMethod = symbolCipher;
-  } else if (encryptionType === 'reverse') {
+  } else if (encryptionType === "reverse") {
     encryptionMethod = reverseCipher;
-  } else if (encryptionType === 'caesar') {
+  } else if (encryptionType === "caesar") {
     let amount = Number(process.argv[3]);
     if (Number.isNaN(amount)) {
-      process.stdout.write(`Try again with a valid amount argument. \n`)
-      process.exit();  
+      process.stdout.write(`Try again with a valid amount argument. \n`);
+      process.exit();
     }
     encryptionMethod = (str) => caesarCipher(str, amount);
-  } 
-  else {
-    process.stdout.write(`Try again with a valid encryption type. \n`)
+  } else {
+    process.stdout.write(`Try again with a valid encryption type. \n`);
     process.exit();
   }
 
-  process.stdout.write('Enter the message you would like to encrypt...\n> ');
+  process.stdout.write("Enter the message you would like to encrypt...\n> ");
   return encryptionMethod;
 }
 
 /* Helper function for displaying the encrypted message to the user. */
 function displayEncryptedMessage(encryptionMethod, userInput) {
-  let str = userInput.toString().trim();    
+  let str = userInput.toString().trim();
   let output = encryptionMethod(str);
-  process.stdout.write(`\nHere is your encrypted message:\n> ${output}\n`)
+  process.stdout.write(`\nHere is your encrypted message:\n> ${output}\n`);
   process.exit();
 }
