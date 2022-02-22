0dba96ecf8cae917890fb3ad942fee406306339b
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-22 13:45:03 +0000

    Update MainController.js

--- Angular/MOVElog/js/controllers/MainController.js
+++ Angular/MOVElog/js/controllers/MainController.js
@@ -1,32 +1,32 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.exercises = [
     {
-      icon: 'img/pushup.jpg',
+      icon: 'https://3ada6b6c0c7e4e78ac174a76cce40b3e.cc-propeller.cloud/img/pushup.jpg',
       name: 'Pushups',
       count: 20
     },
     {
-      icon: 'img/squat.jpg',
+      icon: 'https://3ada6b6c0c7e4e78ac174a76cce40b3e.cc-propeller.cloud/img/squat.jpg',
       name: 'Squats',
       count: 15
     },
     {
-      icon: 'img/pullup.jpg',
+      icon: 'https://3ada6b6c0c7e4e78ac174a76cce40b3e.cc-propeller.cloud/img/pullup.jpg',
       name: 'Pullups',
       count: 10
     },
     {
-      icon: 'img/row.jpg',
+      icon: 'https://3ada6b6c0c7e4e78ac174a76cce40b3e.cc-propeller.cloud/img/row.jpg',
       name: 'Rows',
       count: 10
     },
     {
-      icon: 'img/lunge.jpg',
+      icon: 'https://3ada6b6c0c7e4e78ac174a76cce40b3e.cc-propeller.cloud/img/lunge.jpg',
       name: 'Lunges',
       count: 10
     },
     {
-      icon: 'img/stepup.jpg',
+      icon: 'https://3ada6b6c0c7e4e78ac174a76cce40b3e.cc-propeller.cloud/img/situp.jpg',
       name: 'Step Ups',
       count: 10
     },
