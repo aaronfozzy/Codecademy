379d92a7c68cae8595a806f33ca781d12d3b36bf
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-02-23 16:58:48 +0000

    Delete Angular/Outbox 1/services directory

--- Angular/Outbox 1/services/emails.js
+++
@@ -1,9 +0,0 @@
-app.factory('emails', ['$http', function($http) { 
-  return $http.get('https://content.codecademy.com/courses/ltp4/emails-api/emails.json') 
-            .success(function(data) { 
-              return data; 
-            }) 
-            .error(function(err) { 
-              return err; 
-            }); 
-}]);
