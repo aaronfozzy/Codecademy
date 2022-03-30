c2d0733f0eb3abb2bcfa3fcb4dcf3263319502fd
aaronfozzy <86562735+aaronfozzy@users.noreply.github.com>
2022-03-30 12:46:39 +0100

    Delete mytriathlon-automated-frontend-testing-6dffb50d8c49 directory

--- mytriathlon-automated-frontend-testing-6dffb50d8c49/run-tests.sh
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-casperjs  tests/mytriathloncheckoutbodyglide-live.js
-=======
-casperjs  test tests/mytriathloncheckoutbodyglide-live.js;
-casperjs tests/mytriathlonhomepage-live.js;
-casperjs tests/mytriathlonOverseasDelivery-live.js;
-casperjs tests/mytriathlonPersonalService-live.js;
-casperjs tests/mytriathlonTerms-live.js;
->>>>>>> RELEASE-aaron
